cc7105aacb588621b785b205f7b6a309b08527e8
Alice Sedgwick <101151775+AxisShine@users.noreply.github.com>
2024-10-21 21:10:37 -0300

    Having errors, but added light script to light component

--- Assets/Scripts/InterrogationLight.cs
+++ Assets/Scripts/InterrogationLight.cs
@@ -22,41 +22,41 @@ public class InterrogationLight : MonoBehaviour
             light.color.red = 255;
             light.color.blue = 0;
         } else if (trust == 1) {
-            light.color.red = 229.5;
-            light.color.blue = 25.5;
+            light.color.red = 229;
+            light.color.blue = 25;
         }
         else if (trust == 2) {
             light.color.red = 204;
             light.color.blue = 51;
         }
         else if (trust == 3) {
-            light.color.red = 178.5;
-            light.color.blue = 76.5;
+            light.color.red = 178;
+            light.color.blue = 76;
         }
         else if (trust == 4) {
             light.color.red = 153;
             light.color.blue = 102;
         }
         else if (trust == 5) {
-            light.color.red = 127.5;
-            light.color.blue = 127.5;
+            light.color.red = 127;
+            light.color.blue = 127;
         }
         else if (trust == 6) {
             light.color.red = 102;
             light.color.blue = 153;
         }
         else if (trust == 7) {
-            light.color.red = 76.5;
-            light.color.blue = 178.5;
+            light.color.red = 76;
+            light.color.blue = 178;
         }
         else if (trust == 8) {
             light.color.red = 51;
             light.color.blue = 204;
         }
         else if (trust == 9) {
-            light.color.red = 25.5;
-            light.color.blue = 229.5;
-        } else (trust == 10) {
+            light.color.red = 25;
+            light.color.blue = 229;
+        } else {
             light.color.red = 0;
             light.color.blue = 255;
         }
