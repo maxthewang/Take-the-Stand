4df3d2cddb60cdd83a19c2a0643b5676792ca8dd
JG-e <zezhengh@163.com>
2024-10-23 10:41:41 -0400

    Added Scene flow to start scene

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUIHandler : MonoBehaviour
+{
+    GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameManager.instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame(){
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame(){
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
