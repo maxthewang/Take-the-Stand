4fcaaf2b911df8604a93f49a10413eed88921f52
maxthewang <maxthe.wang@mail.utoronto.ca>
2025-04-11 22:54:37 -0400

    Prevented left joystick drift, more button indicators, notepad last page clears

--- Assets/Scripts/ClosePopup.cs
+++ Assets/Scripts/ClosePopup.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class ClosePopup : MonoBehaviour
 {
-    public Image popupImage;
+    public UnityEngine.UI.Image popupImage;
+    public UnityEngine.UI.Image xButtonImage;
     public float fadeDuration = 1f;
     private PlayerInputActions playerControls;
     private InputAction startAction;
@@ -52,20 +54,24 @@ public class ClosePopup : MonoBehaviour
     IEnumerator FadeOut()
     {
         // Get the current color of the image
-        Color imageColor = popupImage.color;
-        float startAlpha = imageColor.a;
+        Color popupColor = popupImage.color;
+        Color xButtonColor = xButtonImage.color;
+        float startAlpha = popupColor.a;
 
         // Gradually reduce the alpha value over time
         for (float t = 0f; t < fadeDuration; t += Time.deltaTime)
         {
             float normalizedTime = t / fadeDuration;
             // Set the alpha based on the normalized time (t/duration)
-            imageColor.a = Mathf.Lerp(startAlpha, 0f, normalizedTime);
-            popupImage.color = imageColor;
+            popupColor.a = Mathf.Lerp(startAlpha, 0f, normalizedTime);
+            popupImage.color = popupColor;
+            xButtonColor.a = Mathf.Lerp(startAlpha, 0f, normalizedTime);
+            xButtonImage.color = xButtonColor;
 
             yield return null; // Wait for the next frame
         }
         
         popupImage.gameObject.SetActive(false);
+        xButtonImage.gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/PlayerInputActions.cs
+++ Assets/Scripts/PlayerInputActions.cs
@@ -79,7 +79,7 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
                     ""id"": ""978bfe49-cc26-4a3d-ab7b-7d7a29327403"",
                     ""path"": ""<Gamepad>/leftStick"",
                     ""interactions"": """",
-                    ""processors"": """",
+                    ""processors"": ""StickDeadzone(min=0.2)"",
                     ""groups"": "";Gamepad"",
                     ""action"": ""Move"",
                     ""isComposite"": false,
